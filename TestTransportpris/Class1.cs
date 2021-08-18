a66927423b116065adebbf75226ce487ff31dbe5
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2021-08-18 14:17:40 +0200

    Add project files.

---
+++ TestTransportpris/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transportpris;
+using Xunit;
+
+namespace TestTransportpris
+{
+    public class TransportBeregnerTest
+    {
+        [InlineData(4, 4, 0)]
+        [InlineData(4, 10, 50)]
+        [InlineData(5, 4, 75)]
+        [InlineData(5, 10, 100)]
+        [Theory]
+        public void Kan_beregne_med_gyldige_data(int afstand, int vaegt, int forventetPris)
+        {
+            // Arrange
+            TransportBeregner beregner = new TransportBeregner();
+
+            // Act
+            int beregnetPris = beregner.getTransportPris(afstand, vaegt);
+
+            // Assert
+            Assert.Equal(forventetPris, beregnetPris);
+        }
+    }
+}
---
+++ Transportpris/TransportBeregner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportpris
+{
+    public class TransportBeregner
+    {
+        /*
+         * Afstand   Vægt       Pris
+         * < 5 km   < 10 kg     0
+         * < 5 km   >= 10 kg    50
+         * >= 5km   < 10 kg     75
+         * >= 5km   >= 10 kg    100
+         */
+        public int getTransportPris(int afstand, int vaegt)
+        {
+            if (afstand < 5 && vaegt < 10)
+            {
+                return 0;
+            }
+            if (afstand < 5)
+            {
+                return 50;
+            }
+
+            if (vaegt < 10)
+            {
+                return 75;
+            }
+
+            return 100;
+        }
+    }
+}
